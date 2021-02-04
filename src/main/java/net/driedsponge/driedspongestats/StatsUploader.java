7f9dc1c3be6f16581949e1fc58d5c5545e72893b
DriedSponge <jordan@driedsponge.net>
2021-02-03 18:37:59 -0800

    Request send json now. don't need any of that dumb multi part encoding shit!

--- src/main/java/net/driedsponge/driedspongestats/StatsUploader.java
+++ src/main/java/net/driedsponge/driedspongestats/StatsUploader.java
@@ -7,11 +7,15 @@ import net.minecraftforge.common.config.Config;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -36,41 +40,56 @@ public class StatsUploader {
             String finalstats = gson.toJson(stats);
 
             try{
-                Map<String,Object> params = new LinkedHashMap<>();
+
+                Map<String, String> params = new HashMap<String, String>();
                 params.put("username", name);
                 params.put("uuid", UUID);
                 params.put("stats", finalstats);
 
-                StringBuilder postData = new StringBuilder();
-                for (Map.Entry<String,Object> param : params.entrySet()) {
-                    if (postData.length() != 0) postData.append('&');
-                    postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
-                    postData.append('=');
-                    postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
-                }
+                String jsonInputString = gson.toJson(params);
+
+
                 DriedSpongeStats.logger.info("Posting stats for "+name+" to api..");
-                byte[] postDataBytes = postData.toString().getBytes("UTF-8");
                 URL url = new URL(ApiURL);
                 connection = (HttpURLConnection) url.openConnection();
                 connection.setRequestMethod("POST");
                 connection.setReadTimeout(5000);
                 connection.setReadTimeout(50000);
                 connection.setRequestProperty("Authorization","Bearer "+TOKEN);
-                connection.setRequestProperty("Content-Type", "multipart/form-data");
+                connection.setRequestProperty("Content-Type", "application/json; utf-8");
+                connection.setRequestProperty("Accept", "application/json");
                 connection.setDoOutput(true);
 
-                connection.getOutputStream().write(postDataBytes);
+                try(OutputStream os = connection.getOutputStream()) {
+                    byte[] input = jsonInputString.getBytes(StandardCharsets.UTF_8);
+                    os.write(input, 0, input.length);
+                }
 
                 int status = connection.getResponseCode();
 
                 if(ModConfig.ApiConfig.DEBUG){
-                    DriedSpongeStats.logger.info(connection.getResponseMessage());
+                    String strCurrentLine;
+                    BufferedReader br;
+
+                    if(status >= 200 && status <= 299){
+                        br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                        while ((strCurrentLine = br.readLine()) != null) {
+                            DriedSpongeStats.logger.info(strCurrentLine);
+                        }
+                    }else{
+                        br = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+                        while ((strCurrentLine = br.readLine()) != null) {
+                            DriedSpongeStats.logger.info(strCurrentLine);
+                        }
+                    }
+
                     DriedSpongeStats.logger.info("Stats: "+finalstats);
                     DriedSpongeStats.logger.info("Player UUID: "+UUID);
                     DriedSpongeStats.logger.info("Player Username: "+name);
+                    DriedSpongeStats.logger.info("Entire JSON: "+jsonInputString);
                 }
 
-                DriedSpongeStats.logger.info("Api responded with "+status);
+                DriedSpongeStats.logger.info("Api responded with "+status+" ("+connection.getResponseMessage()+")");
                 DriedSpongeStats.logger.info("Stats have been posted for "+name);
 
             }catch (MalformedURLException e){
