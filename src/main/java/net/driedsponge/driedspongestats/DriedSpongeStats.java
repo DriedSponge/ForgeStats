4412bff3ce99a2aa6b3965c8df4029a45550b3ea
DriedSponge <jordan@driedsponge.net>
2021-02-03 18:38:59 -0800

    Updated version

--- src/main/java/net/driedsponge/driedspongestats/DriedSpongeStats.java
+++ src/main/java/net/driedsponge/driedspongestats/DriedSpongeStats.java
@@ -13,7 +13,7 @@ import org.apache.logging.log4j.Logger;
 public class DriedSpongeStats {
     public static final String MODID = "driedspongestats";
     public static final String NAME = "DriedSponge Stats";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
 
     public static Logger logger;
 
